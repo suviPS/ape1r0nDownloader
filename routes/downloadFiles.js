0a79ed13a4e42a58246aefe2a29d86f19dd06b37
Petar Suvajac <petars38@gmail.com>
2021-02-23 18:44:41 +0100

    Handle no params passed

--- routes/downloadFiles.js
+++ routes/downloadFiles.js
@@ -4,9 +4,9 @@ const request = require('sync-request');
 const fs = require('fs');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
-//ID : Type : Name : URL 
 const csvWriter = createCsvWriter({
     path: 'listFilesResult.csv',
+    //ID : Type : Name : URL 
     header: [
       {id: 'id', title: 'ID'},
       {id: 'type', title: 'File type'},
@@ -14,36 +14,38 @@ const csvWriter = createCsvWriter({
       {id: 'url', title: 'File URL'},
     ],
     //if set appends to an existing file, skips header write 
-    append: true
+    // append: true
   });
 
 const baseUrl = 'http://www.apeiron-uni.com/lycboardclient/Detail.aspx?DocumentID=';
 
-/* GET List of files with titles */
+/* GET Download files */
 router.get('/', async function(req, res, next) {
-    //todo add params
+    //get params
     const numberOfFiles = req.query.numOfFiles;
     const lastFileNumber = req.query.lastFile;
-    var url;
 
     if(req.query.numOfFiles == undefined || req.query.lastFile == undefined){
         res.send('Please provide numOfFiles and lastFile as query params');
+        return;
     }
     
     const fileList = [];
+    var url;
     for(var i= lastFileNumber; i > lastFileNumber - numberOfFiles; i--){
         url = baseUrl + i;
         var apeironRes = request('GET', url);
         console.log('Req for: ' + i);
-        console.log(apeironRes.headers['content-length']);
         if(apeironRes.headers['content-disposition']){
             //file
             console.log(apeironRes.headers['content-disposition']);
 
-            //header: attachment; filename=XXX.XX
+            //name header: attachment; filename=XXX.XX
             var name = apeironRes.headers['content-disposition'].split(';')[1].substring(10);
             //replace forward slash in file name with dash
             name = name.replace(/\//g, '-');
+
+            //type header
             var type = apeironRes.headers['content-type'];
 
             //save file (already available in apeironRes.body)
@@ -67,8 +69,8 @@ router.get('/', async function(req, res, next) {
             console.log('- No file for url: ' + url);
             var fileEntry = {
                 id: i,
-                type: '',
-                name: '',
+                type: '//',
+                name: '//',
                 url: url
             }
             fileList[lastFileNumber - i] = fileEntry;
@@ -83,14 +85,12 @@ router.get('/', async function(req, res, next) {
     //     .then(()=> {
     //         console.log('The CSV file was written successfully')
     //     });
-    
+
     await csvWriter.writeRecords(fileList);
     console.log('The CSV file was written successfully');
 
     // res.send(fileList);
     res.send('Processing done, check <b>listFilesResult.csv</b> in project folder.');
 });
-
-
 
 module.exports = router;
